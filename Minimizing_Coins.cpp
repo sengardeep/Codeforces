ad5487f75a501eb39ab3c2647903b194db366ce9
sengardeep <sengardeep2006@gmail.com>
2025-07-27 09:58:00 +0530

    feat: add Coin_Combinations_I and Coin_Combinations_II solutions, ignore .vscode and .txt files

---
+++ Minimizing_Coins.cpp
@@ -0,0 +1,117 @@
+#include <bits/stdc++.h>
+#include <ext/pb_ds/assoc_container.hpp>
+#include <ext/pb_ds/tree_policy.hpp>
+
+#define int long long
+#define MOD 1000000007
+#define fastio() (ios_base::sync_with_stdio(false), cin.tie(NULL))
+using namespace std;
+using namespace __gnu_pbds;
+
+// Common Macros
+#define _GLIBCXX_INCLUDE_NEXT_C_HEADERS
+#define vi vector<int>
+#define vvi vector<vi>
+#define all(v) v.begin(), v.end()
+#define pb push_back
+#define FOR(i, start, end) for (int i = start; i < end; ++i)
+#define dbg(x) cerr << #x << " = " << (x) << endl
+#define YES cout << "YES" << endl;
+#define NO cout << "NO" << endl;
+#define MOD_INV(x) mod_exp(x, MOD - 2, MOD)
+typedef tree<int, null_type, less<int>, rb_tree_tag, tree_order_statistics_node_update> pbds; // find_by_order, order_of_key
+
+// Debugging Helpers
+template <typename T> void dbg_vec(const T &v) {
+    cerr << "[ ";
+    for (const auto &x : v) cerr << x << " ";
+    cerr << "]" << endl;
+}
+
+// Utility Functions
+int gcd(int a, int b) { return b ? gcd(b, a % b) : a; }
+int lcm(int a, int b) { return (a / gcd(a, b)) * b; }
+bool is_prime(int n) {
+    if (n <= 1) return false;
+    for (int i = 2; i * i <= n; ++i) if (n % i == 0) return false;
+    return true;
+}
+int mod_exp(int base, int exp, int mod = MOD) {
+    int result = 1;
+    while (exp) {
+        if (exp % 2 == 1) result = (result * base) % mod;
+        base = (base * base) % mod;
+        exp /= 2;
+    }
+    return result;
+}
+//Combatronics
+//Uncomment if needed
+
+// const int N = 1e6 + 1;
+// vector<int> fact(N,1);
+// vector<int> inv_fact(N,1);
+// void factorial(){
+//     for(int i=2;i<N;i++){
+//         fact[i]=(fact[i-1]*i)%MOD;
+//     }
+// }
+// void invFactorial(){
+//     inv_fact[N-1]=MOD_INV(fact[N-1]);
+//     for(int i=N-2;i>=0;i--){
+//         inv_fact[i]=(inv_fact[i+1]*(i+1))%MOD;
+//     }
+// }
+// int nCr(int n,int r,int mod){
+//     return (((fact[n]*inv_fact[n-r])%mod)*inv_fact[r])%mod;
+// }
+
+// Fast Input/Output Functions
+template<typename T> void read(T& x) { cin >> x; }
+template<typename T, typename... Args>
+void read(T& first, Args&... rest) {
+    cin >> first;
+    read(rest...);
+}
+template<typename T> void read(vector<T>& v) { for (auto &x : v) cin >> x; }
+template<typename T> void print(const vector<T>& v) { for (auto &x : v) cout << x << " "; cout << endl; }
+
+
+// Created by Deep 
+// Date : 26-07-2025 
+// Time : 14:49
+
+// Solution Function
+void solve() {
+    int n,k;
+    read(n,k);
+    vi v(n);
+    read(v);
+
+    vi dp(1e6+1,1e9);
+    dp[0]=0;
+    //dp[i] is equal to minimum coins required to generate sum of i 
+    FOR(i,1,k+1){
+        FOR(j,0,n){
+            if(i>=v[j]){
+                dp[i]=min(dp[i],dp[i-v[j]]+1);
+            }
+        }
+    }
+
+    cout<<((dp[k]<1e9)?dp[k]:-1)<<endl;
+}
+
+// Main Function
+int32_t main() {
+    fastio();
+    // memset(dp,-1,sizeof(dp));
+    #ifdef LOCAL
+        freopen("input.txt", "r", stdin);
+        freopen("output.txt", "w", stdout);
+    #endif
+    int t = 1;
+    // cin >> t;
+    while (t--) solve();
+    return 0;
+}
