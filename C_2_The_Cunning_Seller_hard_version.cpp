69b79365221f7c7a1905a1b17fddfde6734f574a
sengardeep <sengardeep2006@gmail.com>
2025-08-21 22:59:20 +0530

    feat: Add A_Homework and B_The_Secret_Number solutions

---
+++ C_2_The_Cunning_Seller_hard_version.cpp
@@ -0,0 +1,138 @@
+#include <bits/stdc++.h>
+#include <ext/pb_ds/assoc_container.hpp>
+#include <ext/pb_ds/tree_policy.hpp>
+
+#define int long long
+#define MOD 1000000007
+#define fastio() (ios_base::sync_with_stdio(false), cin.tie(NULL))
+using namespace std;
+using namespace __gnu_pbds;
+
+// Common Macros
+#define _GLIBCXX_INCLUDE_NEXT_C_HEADERS
+#define vi vector<int>
+#define vvi vector<vi>
+#define all(v) v.begin(), v.end()
+#define pb push_back
+#define FOR(i, start, end) for (int i = start; i < end; ++i)
+#define dbg(x) cerr << #x << " = " << (x) << endl
+#define YES cout << "YES" << endl;
+#define NO cout << "NO" << endl;
+#define INF LLONG_MAX
+#define MOD_INV(x) mod_exp(x, MOD - 2, MOD)
+typedef tree<int, null_type, less<int>, rb_tree_tag, tree_order_statistics_node_update> pbds; // find_by_order, order_of_key
+
+// Debugging Helpers
+template <typename T> void dbg_vec(const T &v) {
+    cerr << "[ ";
+    for (const auto &x : v) cerr << x << " ";
+    cerr << "]" << endl;
+}
+
+// Utility Functions
+int gcd(int a, int b) { return b ? gcd(b, a % b) : a; }
+int lcm(int a, int b) { return (a / gcd(a, b)) * b; }
+bool is_prime(int n) {
+    if (n <= 1) return false;
+    for (int i = 2; i * i <= n; ++i) if (n % i == 0) return false;
+    return true;
+}
+long long binpow(long long a, long long b) {
+    long long res = 1;
+    while (b > 0) {
+        if (b & 1)
+            res = res * a;
+        a = a * a;
+        b >>= 1;
+    }
+    return res;
+}
+//Combatronics
+//Uncomment if needed
+
+// const int N = 1e6 + 1;
+// vector<int> fact(N,1);
+// vector<int> inv_fact(N,1);
+// void factorial(){
+//     for(int i=2;i<N;i++){
+//         fact[i]=(fact[i-1]*i)%MOD;
+//     }
+// }
+// void invFactorial(){
+//     inv_fact[N-1]=MOD_INV(fact[N-1]);
+//     for(int i=N-2;i>=0;i--){
+//         inv_fact[i]=(inv_fact[i+1]*(i+1))%MOD;
+//     }
+// }
+// int nCr(int n,int r,int mod){
+//     return (((fact[n]*inv_fact[n-r])%mod)*inv_fact[r])%mod;
+// }
+
+// Fast Input/Output Functions
+template<typename T> void read(T& x) { cin >> x; }
+template<typename T, typename... Args>
+void read(T& first, Args&... rest) {
+    cin >> first;
+    read(rest...);
+}
+template<typename T> void read(vector<T>& v) { for (auto &x : v) cin >> x; }
+template<typename T> void print(const vector<T>& v) { for (auto &x : v) cout << x << " "; cout << endl; }
+
+
+// Created by Deep 
+// Date : 21-08-2025 
+// Time : 20:47
+
+
+// Solution Function
+void solve() {
+    int n,k;
+    read(n,k);
+    int ans=0;
+    vi vec;
+    while(n>0) {
+        int r = n%3;
+        n/=3;
+        vec.pb(r);
+        k -= r;
+    }
+    if(k<0) {
+        cout<<"-1"<<endl;
+        return;
+    }
+ 
+    while(k>1 && vec.size() > 1) {
+        int c = vec.back();
+        int x = vec.size() - 1;
+        if(2*c > k) {
+            vec[x] -= k/2;
+            vec[x-1] += 3*(k/2);
+            break;
+        }
+        else {
+            vec.pop_back();
+            vec[x-1] += 3*c;
+            k -= 2*c;
+        }
+    }
+    FOR(x,0,vec.size()) {
+        ans += vec[x] * (pow(3, x+1) + x*pow(3, x-1));
+    }
+ 
+ 
+    cout<<ans<<endl;
+    
+}
+
+// Main Function
+int32_t main() {
+    fastio();
+    #ifdef LOCAL
+        freopen("input.txt", "r", stdin);
+        freopen("output.txt", "w", stdout);
+    #endif
+    int t = 1;
+    cin >> t;
+    while (t--) solve();
+    return 0;
+}
