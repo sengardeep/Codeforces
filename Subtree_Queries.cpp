ed9d1f4ae46dc3401f60f74632b2f6947e0e1181
sengardeep <sengardeep2006@gmail.com>
2025-09-10 22:21:52 +0530

    aPath_Queries solution added

---
+++ Subtree_Queries.cpp
@@ -0,0 +1,160 @@
+#include<bits/stdc++.h>
+using namespace std;
+#define int long long
+const int mod=1e9+7;
+#define endl "\n"
+#define dbg(x) cerr<<#x<<" = "<<(x)<<endl
+#define pii pair<int,int>
+template<typename Node, typename Update>
+struct SegTree {
+    vector<Node> tree;
+    vector<int> arr; // type may change
+    int n;
+    int s;
+    SegTree(int a_len, vector<int> &a) { // change if type updated
+        arr = a;
+        n = a_len;
+        s = 1;
+        while(s < 2 * n){
+            s = s << 1;
+        }
+        tree.resize(s); fill(tree.begin(), tree.end(), Node());
+        build(0, n - 1, 1);
+    }
+    void build(int start, int end, int index)  // Never change this
+    {
+        if (start == end)   {
+            tree[index] = Node(arr[start]);
+            return;
+        }
+        int mid = (start + end) / 2;
+        build(start, mid, 2 * index);
+        build(mid + 1, end, 2 * index + 1);
+        tree[index].merge(tree[2 * index], tree[2 * index + 1]);
+    }
+    void update(int start, int end, int index, int query_index, Update &u)  // Never Change this
+    {
+        if (start == end) {
+            u.apply(tree[index]);
+            return;
+        }
+        int mid = (start + end) / 2;
+        if (mid >= query_index)
+            update(start, mid, 2 * index, query_index, u);
+        else
+            update(mid + 1, end, 2 * index + 1, query_index, u);
+        tree[index].merge(tree[2 * index], tree[2 * index + 1]);
+    }
+    Node query(int start, int end, int index, int left, int right) { // Never change this
+        if (start > right || end < left)
+            return Node();
+        if (start >= left && end <= right)
+            return tree[index];
+        int mid = (start + end) / 2;
+        Node l, r, ans;
+        l = query(start, mid, 2 * index, left, right);
+        r = query(mid + 1, end, 2 * index + 1, left, right);
+        ans.merge(l, r);
+        return ans;
+    }
+    void make_update(int index, int val) {  // pass in as many parameters as required
+        Update new_update = Update(val); // may change
+        update(0, n - 1, 1, index, new_update);
+    }
+    Node make_query(int left, int right) {
+        return query(0, n - 1, 1, left, right);
+    }
+};
+struct Node1 {
+    int val; // may change
+    Node1() { // Identity element
+        val = 0;    // may change
+    }
+    Node1(int p1) {  // Actual Node
+        val = p1; // may change
+    }
+    void merge(Node1 &l, Node1 &r) { // Merge two child nodes
+        val = l.val + r.val;  // may change
+    }
+};
+struct Update1 {
+    int val; // may change
+    Update1(int p1) { // Actual Update
+        val = p1; // may change
+    }
+    void apply(Node1 &a) { // apply update to given node
+        a.val = val; // may change
+    }
+};
+void solve() {
+   int n,q;
+   cin>>n>>q;
+   vector<int> v(n+1);
+   for(int i=1;i<=n;i++) cin>>v[i];
+   vector<int> adj[n+1];
+   for(int i=1;i<n;i++){
+    int u,v;
+    cin>>u>>v;
+    adj[u].push_back(v);
+    adj[v].push_back(u);
+   }
+   //Euler Tour (Type - 2)
+   vector<int> flat;
+   map<int,pii> map;  //{Val,(firstOcc,lstOcc)}
+   int cnt=0;
+   function<void(int,int)> dfs=[&](int node,int par)->void{
+    //Entering
+    flat.push_back(node);
+    map[node].first=cnt;
+    cnt++;
+    for(auto child : adj[node]){
+        if(child == par) continue;
+        dfs(child,node);
+    }
+    //Exiting
+    flat.push_back(node);
+    map[node].second=cnt;
+    cnt++;
+   };
+   dfs(1, -1);
+   vector<int> arr(flat.size(),0);  //Store the value of each node at their first index
+   for(int i=1;i<=n;i++){
+    arr[map[i].first]=v[i];
+   }
+   //Apply Segment Tree on arr
+   SegTree<Node1,Update1> tree = SegTree<Node1,Update1>(flat.size(),arr);
+   while(q--){
+    int type;
+    cin>>type;
+    if(type==1){
+        int node,val;
+        cin>>node>>val;
+        int idx = map[node].first;
+        tree.make_update(idx,val);
+    }
+    else{
+        int node;
+        cin>>node;
+        cout<<tree.make_query(map[node].first,map[node].second).val<<endl;
+    }
+   }
+
+}
+
+int32_t main(){
+   ios::sync_with_stdio(0);
+   cin.tie(0);
+
+   #ifdef LOCAL
+   freopen("input.txt","r",stdin);
+   freopen("output.txt","w",stdout);
+   #endif
+
+   int t=1;
+//    cin>>t;
+   while(t--){
+       solve();
+   }
+
+   return 0;
+}
