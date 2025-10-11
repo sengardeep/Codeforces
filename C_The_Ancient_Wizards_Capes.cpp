dc71d4f6e2921c12f01745d31f261343b9ae61bb
sengardeep <sengardeep2006@gmail.com>
2025-10-11 09:07:51 +0530

    Add solution for problem C - 'The Ancient Wizard's Capes'

---
+++ C_The_Ancient_Wizards_Capes.cpp
@@ -0,0 +1,33 @@
+#include<bits/stdc++.h>
+using namespace std;
+#define int long long
+const int mod=676767677;
+#define endl "\n"
+#define dbg(x) cerr<<#x<<" = "<<(x)<<endl
+#define pii pair<int,int>
+#define pb push_back
+
+void solve() {
+    int n;
+    cin>>n;
+    vector<int> v(n);
+    for(int i=0;i<n;i++) cin>>v[i];
+}
+
+int32_t main(){
+   ios::sync_with_stdio(0);
+   cin.tie(0);
+
+   #ifdef LOCAL
+   freopen("input.txt","r",stdin);
+   freopen("output.txt","w",stdout);
+   #endif
+
+   int t=1;
+   cin>>t;
+   while(t--){
+       solve();
+   }
+
+   return 0;
+}
