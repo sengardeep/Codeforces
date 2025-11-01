b72f728b5094711d2289da9358ff3c27c52e80f3
sengardeep <sengardeep2006@gmail.com>
2025-11-01 17:47:26 +0530

    refactor: replace can_reach_all function with solve function, add multiset and pair data structures, implement new logic for handling vip and regular arrivals, and add solution test

--- test.cpp
+++ test.cpp
@@ -1,62 +1,89 @@
-#include <bits/stdc++.h>
+#include <iostream>
+#include <vector>
+#include <set>
+#include <algorithm>
+#include <utility>
+#include <iterator>
 using namespace std;
 
-static inline bool can_reach_all(const vector<long long>& A, long long h){
-    int n = (int)A.size();
-    if(n==0) return true;
-    vector<char> vis(n+1, 0); // 0 = ground, 1..n platforms
-    deque<int> q;
-    // Start from ground: enqueue all platforms reachable from ground
-    for(int i=1;i<=n;i++){
-        if(A[i-1] <= h){
-            vis[i] = 1;
-            q.push_back(i);
-        }
+#define pb push_back
+#define pii pair<int,int>
+#define all(x) begin(x),end(x)
+
+void solve() {
+    int n;
+    cin>>n;
+    vector<pii> regular,vip;
+    multiset<int> arrivals;
+    int curr=INT_MAX;
+    for(int i=0;i<n;i++){
+        int at,pt,p;
+        cin>>at>>pt>>p;
+        if(p) vip.pb(make_pair(at,pt));
+        else regular.pb(make_pair(at,pt));
+        arrivals.insert(at);
+        curr=min(curr,at);
     }
-    int reached = (int)q.size();
-    while(!q.empty()){
-        int i = q.front(); q.pop_front();
-        // i-1 neighbor
-        if(i-1>=1 && !vis[i-1]){
-            if(llabs(A[i-1] - A[i-2]) <= h){
-                vis[i-1] = 1;
-                q.push_back(i-1);
-                reached++;
+    sort(all(vip));     
+    sort(all(regular)); 
+
+    int i=0,j=0;
+    int ans=0;
+
+    for(int k=0;k<n;k++){
+        if(j<vip.size() && i<regular.size()){
+            if(vip[j].first<=curr){
+                curr=max(curr,vip[j].first);
+                curr+=vip[j].second;
+                arrivals.erase(arrivals.find(vip[j].first));
+                ans=max(ans,(int)distance(begin(arrivals), arrivals.lower_bound(curr)));
+                j++;
+            }
+            else if(regular[i].first<=curr){
+                curr=max(curr,regular[i].first);
+                curr+=regular[i].second;
+                arrivals.erase(arrivals.find(regular[i].first));
+                ans=max(ans,(int)distance(begin(arrivals), arrivals.lower_bound(curr)));
+                i++;
+            }
+            else{
+                if (vip[j].first <= regular[i].first) {
+                    curr=max(curr,vip[j].first);
+                    curr+=vip[j].second;
+                    arrivals.erase(arrivals.find(vip[j].first));
+                    ans=max(ans,(int)distance(begin(arrivals), arrivals.lower_bound(curr)));
+                    j++;
+                } else {
+                    curr=max(curr,regular[i].first);
+                    curr+=regular[i].second;
+                    arrivals.erase(arrivals.find(regular[i].first));
+                    ans=max(ans,(int)distance(begin(arrivals), arrivals.lower_bound(curr)));
+                    i++;
+                }
+            }
+        }else{
+            if(j<vip.size()){
+                curr=max(curr,vip[j].first);
+                curr+=vip[j].second;
+                arrivals.erase(arrivals.find(vip[j].first));
+                ans=max(ans,(int)distance(begin(arrivals), arrivals.lower_bound(curr)));
+                j++;
+            }
+            else{
+                curr=max(curr,regular[i].first);
+                curr+=regular[i].second;
+                arrivals.erase(arrivals.find(regular[i].first));
+                ans=max(ans,(int)distance(begin(arrivals), arrivals.lower_bound(curr)));
+                i++;
             }
         }
-        // i+1 neighbor
-        if(i+1<=n && !vis[i+1]){
-            if(llabs(A[i] - A[i-1]) <= h){
-                vis[i+1] = 1;
-                q.push_back(i+1);
-                reached++;
-            }
-        }
     }
-    return reached == n;
+    cout<<ans;
 }
 
-long long solve_binary(const vector<long long>& A){
-    if(A.empty()) return 0;
-    long long maxA = 0, maxDiff = 0;
-    for(long long x: A) maxA = max(maxA, x);
-    for(size_t i=1;i<A.size();i++) maxDiff = max(maxDiff, llabs(A[i]-A[i-1]));
-    long long lo = 0, hi = max(maxA, maxDiff);
-    while(lo < hi){
-        long long mid = (lo + hi) >> 1;
-        if(can_reach_all(A, mid)) hi = mid;
-        else lo = mid + 1;
-    }
-    return lo;
-}
-
-int main(){
+int main() {
     ios::sync_with_stdio(false);
-    cin.tie(nullptr);
-    int N;
-    if(!(cin>>N)) return 0;
-    vector<long long> A(N);
-    for(int i=0;i<N;i++) cin>>A[i];
-    cout << solve_binary(A) << "\n";
+    cin.tie(NULL);
+    solve();
     return 0;
 }
