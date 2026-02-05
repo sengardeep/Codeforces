4485d47f79657ed9bf803674b2a366ac396637ee
sengardeep <sengardeep2006@gmail.com>
2026-02-05 15:52:55 +0530

    refactor: update tasks.json to change default build task and add new C/C++ build task, modify C_The_Ancient_Wizards_Capes solution added, add vc140 pdb file

--- C_The_Ancient_Wizards_Capes.cpp
+++ C_The_Ancient_Wizards_Capes.cpp
@@ -12,6 +12,23 @@ void solve() {
     cin>>n;
     vector<int> v(n);
     for(int i=0;i<n;i++) cin>>v[i];
+
+    auto helper=[&](int k){
+        vector<int> c(n);
+        c[0]=k;
+        for(int i=0;i<n-1;i++){
+            int d=v[i+1]-v[i];
+            c[i+1]=1-d-c[i];
+            if(c[i+1]!=0 && c[i+1]!=1) return 0;
+        }
+        int cnt=0;
+        for(int i=0;i<n;i++){
+            if((c[i]==0 && i==0)||(c[i]==1 && i>=0)) cnt++;
+        }
+        return (cnt==v[0])?1:0;
+    };
+    int ans=helper(0)+helper(1);
+    cout<<ans<<endl;
 }
 
 int32_t main(){
