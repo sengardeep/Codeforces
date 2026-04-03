1a848f8fdc5efab346a00a9ca58e48daf633c07f
sengardeep <sengardeep2006@gmail.com>
2026-04-03 10:36:11 +0530

    Solution added

---
+++ C_1_Lost_Civilization_Easy_Version.cpp
@@ -0,0 +1,41 @@
+#include<bits/stdc++.h>
+using namespace std;
+#define int long long
+const int mod=1e9+7;
+#define endl "\n"
+#define dbg(x) cerr<<#x<<" = "<<(x)<<endl
+#define pii pair<int,int>
+#define pb push_back
+#define all(v) v.begin(),v.end()
+
+void solve() {
+   int n;
+   cin>>n;
+   vector<int> v(n);
+   for(int i=0;i<n;i++) cin>>v[i];
+   stack<int> stack;
+   stack.push(v[n-1]);
+   for(int i=n-2;i>=0;i--){
+    while(!stack.empty() && v[i]+1==stack.top()) stack.pop();
+    stack.push(v[i]);
+   }
+   cout<<stack.size()<<endl;
+}
+
+int32_t main(){
+   ios::sync_with_stdio(0);
+   cin.tie(0);
+
+   #ifdef LOCAL
+   freopen("input.txt","r",stdin);
+   freopen("output.txt","w",stdout);
+   #endif
+
+   int t=1;
+   cin>>t;
+   while(t--){
+       solve();
+   }
+
+   return 0;
+}
